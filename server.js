2b0a46c0ce7b1c80e0de82f3a2a66879ffb5c6b9
HedgehogKU <ccc908925@gmail.com>
2022-04-16 12:51:06 +0800

    新增 router ( Home Page & Not Found Page )

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ const http = require('http');
 const PORT = 3005;
 
 function requestListener(req, res) {
-    res.end();
+    if ( req.url === '/' && req.method === 'GET' ) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h1>Home Page</h1>');
+        res.end();
+    } else if ( req.method === 'OPTIONS' ) {
+        res.writeHead(200);
+        res.end();
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write('<h1>Not Found Page</h1>');
+        res.end();
+    }
 }
 
 const server = http.createServer(requestListener);
